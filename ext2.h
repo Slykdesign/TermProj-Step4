983979401fd720cb09ad89ac49563a7610f91b7e
Hayden Slyk <132722612+Slykdesign@users.noreply.github.com>
2025-04-07 20:48:39 +0000

    Update ext2.h

--- ext2.h
+++ ext2.h
@@ -76,34 +76,6 @@ typedef struct {
     uint32_t num_block_groups;
 } Ext2File;
 
-typedef struct {
-    // Fields for inode structure...
-    uint16_t i_mode;
-    uint16_t i_uid;
-    uint32_t i_size;
-    uint32_t i_atime;
-    uint32_t i_ctime;
-    uint32_t i_mtime;
-    uint32_t i_dtime;
-    uint16_t i_gid;
-    uint16_t i_links_count;
-    uint32_t i_blocks;
-    uint32_t i_flags;
-    uint32_t i_block[15];
-    uint32_t i_generation;
-    uint32_t i_file_acl;
-    uint32_t i_dir_acl;
-    uint32_t i_faddr;
-} Inode;
-
-// New function prototypes
-int32_t fetchInode(Ext2File *f, uint32_t iNum, Inode *buf);
-int32_t writeInode(Ext2File *f, uint32_t iNum, Inode *buf);
-int32_t inodeInUse(Ext2File *f, uint32_t iNum);
-uint32_t allocateInode(Ext2File *f, int32_t group);
-int32_t freeInode(Ext2File *f, uint32_t iNum);
-void displayInode(Inode *inode);
-
 Ext2File *openExt2File(const char *filename);
 void closeExt2File(Ext2File *f);
 bool fetchBlock(Ext2File *f, uint32_t blockNum, void *buf);
@@ -113,6 +85,6 @@ bool writeSuperblock(Ext2File *f, uint32_t blockNum, Ext2Superblock *sb);
 bool fetchBGDT(Ext2File *f, uint32_t blockNum, Ext2BlockGroupDescriptor *bgdt);
 bool writeBGDT(Ext2File *f, uint32_t blockNum, Ext2BlockGroupDescriptor *bgdt);
 void displaySuperblock(Ext2Superblock *sb);
-void displayBlockGroupDescriptorTable(Ext2BlockGroupDescriptor *bgdt, uint32_t num_block_groups);
+void displayBGDT(Ext2BlockGroupDescriptor *bgdt, uint32_t num_block_groups);
 
 #endif
